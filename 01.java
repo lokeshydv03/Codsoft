71250967cd9086068450d70b42e45a4c2e9248f8
lokeshydv03 <132387227+lokeshydv03@users.noreply.github.com>
2023-11-16 12:23:39 +0530

    Add files via upload

---
+++ 01.java
@@ -0,0 +1,8 @@
+public class Javabasics{
+    public static void printhelloworld{
+        System.out.println("hello world");
+    }
+    public static void main(String args[]){
+      printhelloworld();
+    }
+}
